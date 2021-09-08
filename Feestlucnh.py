d357a171e7ee4e628dc69c38a407e53056bd97dc
Timo de Gruiter <timo@EDGITNEW.local>
2021-09-08 12:06:37 +0200

    person verberterd

---
+++ Feestlucnh.py
@@ -0,0 +1,10 @@
+croissantje = 0.39
+stokbrood = 2.78
+kortingsbon = -0.50
+
+
+print (croissantje * 17 )
+print (stokbrood * 2)
+print (kortingsbon * 3)
+print (croissantje * 17 + stokbrood * 2 + kortingsbon *3)
+print('De feestlunch kost je bij de bakker 18.88 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
---
+++ speelhal-dag.py
@@ -0,0 +1,7 @@
+toegangsticket = 7.45
+VIP = 0.37
+
+print(toegangsticket * 3 )
+print(VIP * 9)
+print (VIP * 9 + toegangsticket * 3)
+print('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro')
